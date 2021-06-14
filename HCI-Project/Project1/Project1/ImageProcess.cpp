d0a4ce15dd5eb74b7b762d48d5b1413a49f1457f
HeeJeongOh <62127798+HeeJeongOh@users.noreply.github.com>
2021-06-14 13:25:20 +0900

    Update ImageProcess.cpp

--- HCI-Project/Project1/Project1/ImageProcess.cpp
+++ HCI-Project/Project1/Project1/ImageProcess.cpp
@@ -1,45 +1,40 @@
-﻿#include "ImageProcess.h"
+#include "ImageProcess.h"
 using namespace std;
 
-Mat extract(Mat img) {
-	//2. Convert to gray and normalize
+Mat extract(Mat img, int idx) {
+
+	//1. Convert to gray and normalize
 	Mat gray(img.rows, img.cols, CV_8UC1);
+
 	cvtColor(img, gray, COLOR_BGR2GRAY);
-	imshow("gray", gray);
 
 	// anchor SE의 중심점
 	// kernel이 커질수록 뚜렷해지지만 노이즈도 함께 증가
-	Mat kernel = getStructuringElement(MORPH_ELLIPSE, cv::Size(20, 20), cv::Point(1, 1));
+	Mat kernel = getStructuringElement(MORPH_CROSS, cv::Size(7, 7), cv::Point(3, 3));
 
 	// step1 :: erosion
 	Mat erosion;
 	erode(gray, erosion, kernel);
-	imshow("erosion", erosion);
 
 	// step2 :: opening - dilation after erosion
 	Mat opening;
 	dilate(erosion, opening, kernel);
-	imshow("opening", opening);
-	cvtColor(opening, opening, COLOR_GRAY2BGR);
 
-	// step3 :: tophat
-	Mat tophat = img - opening;
-	imshow("tophat", tophat);
+	Mat tophat;
+	if (idx == 1) {
+		cvtColor(opening, opening, COLOR_GRAY2BGR);
+		tophat = img - opening;
+	}
+	else{
+		// step3 :: closing - erosion after dilation
+		Mat closing;
+		erode(opening, closing, kernel);
 
-	// img2_fg를 이진화 (4)
-	Mat binary_img;
-	threshold(tophat, binary_img, 240, 255, THRESH_BINARY_INV);
-	imshow("binary_img", binary_img);
+		// - 연산을 위해 채널 맞추기
+		cvtColor(closing, closing, COLOR_GRAY2BGR);
+		tophat = img - closing;
+	}
 
-	// img2_fg bitwise_not으로 흑백 바꿈 (5) 
-	Mat not_img;
-	bitwise_not(binary_img, not_img);
-	imshow("not_img", not_img);
-
-	Mat black;
-	// (2) + (4)를 and 연산 (6) 
-	bitwise_and(tophat, binary_img, black); // -> 검정 배경에 이미지만 
-	imshow("black", black);
-
-	return 	black;
+	return tophat;
 }
+
